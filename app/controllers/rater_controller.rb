3c117e2a5563084aa8a640bec96accd792d59a34
Jon Lim <jonlimtoronto@gmail.com>
2015-04-07 16:10:59 -0400

    Making changes per @hesham's review notes. All is well.

--- app/controllers/rater_controller.rb
+++ app/controllers/rater_controller.rb
@@ -1,38 +1,14 @@
 class RaterController < ApplicationController
   
   def index
-    @person = Person.all
+    @people = Person.all
+    
+    offset = rand(@people.size)
+    @current_person = Person.offset(offset).first
   end
   
   def show
     @person = Person.find(params[:id])
-  end
-  
-  def new
-    @person = Person.new
-  end
-  
-  def create
-    @person = Person.new(person_params)
-    @person.save
-  end
-  
-  def edit
-    @person = Person.find(params[:id])
-  end
-  
-  def update
-    @person = Person.find(params[:id])
-    @person.update_attributes(person_params)
-  end
-  
-  def delete
-    @person = Person.find(params[:id])
-  end
-  
-  def destroy
-    Person.find(params[:id]).destroy
-    redirect_to(:action => 'index')
   end
   
   def hot
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,7 @@
 class Person < ActiveRecord::Base
+  
+  def hotness
+    self.times_rated != 0 ? ((self.times_liked.to_f / self.times_rated) * 100).round(2) : 0.00
+  end
+  
 end
