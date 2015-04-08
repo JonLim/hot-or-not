a1ab44cb95abb6e10f59c00a2436d1264a174e52
Jon Lim <jonlimtoronto@gmail.com>
2015-04-08 15:47:04 -0400

    Taking a crack at unit tests for the Person model. Will review.

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,4 +1,5 @@
 class Person < ActiveRecord::Base
+  validates :name, :imageurl, presence: true
   
   def hotness
     self.times_rated != 0 ? ((self.times_liked.to_f / self.times_rated) * 100).round(2) : 0.00
--- test/models/person_test.rb
+++ test/models/person_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class PersonTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "person_count_test" do
+    assert_equal 1, Person.count
+  end
+  
+  test "person_name_not_nil" do
+    assert_not_nil people(:person_one).name
+  end
+  
+  test "person_imageurl_not_nil" do
+    assert_not_nil people(:person_one).imageurl
+  end
+  
+  test "person_hotness_test" do
+    hotness = people(:person_one).times_rated != 0 ? ((people(:person_one).times_liked.to_f / people(:person_one).times_rated) * 100).round(2) : 0.00
+    assert_equal hotness, people(:person_one).hotness
+  end
+  
 end
