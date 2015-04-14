dbfbb5c4aedb329bc8ecff3d38ae0fdd49b20384
Jon Lim <jonlimtoronto@gmail.com>
2015-04-14 18:27:31 -0400

    Committing an initial change to unit tests for review, will change after getting review notes.

--- test/models/person_test.rb
+++ test/models/person_test.rb
@@ -1,9 +1,6 @@
 require 'test_helper'
 
 class PersonTest < ActiveSupport::TestCase
-  test "person_count_test" do
-    assert_equal 1, Person.count
-  end
   
   test "person_name_not_nil" do
     assert_not_nil people(:person_one).name
@@ -15,6 +12,22 @@ class PersonTest < ActiveSupport::TestCase
   
   test "person_times_liked_more_than_rated" do
     assert_not people(:person_one).times_liked > people(:person_one).times_rated
+  end
+  
+  test "person_hot_rated_correct_increment" do
+    original_times_liked = people(:person_one).times_liked
+    original_times_rated = people(:person_one).times_rated
+    people(:person_one).hot
+    assert_equal original_times_liked + 1, people(:person_one).times_liked
+    assert_equal original_times_rated + 1, people(:person_one).times_rated
+  end
+  
+  test "person_not_rated_correct_increment" do
+    original_times_liked = people(:person_one).times_liked
+    original_times_rated = people(:person_one).times_rated
+    people(:person_one).not
+    assert_equal original_times_liked, people(:person_one).times_liked
+    assert_equal original_times_rated + 1, people(:person_one).times_rated
   end
   
   test "person_hotness_test" do
